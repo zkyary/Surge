9147ce065db884c0e8158309d9bd5b6a56b60aed
简单帅 <80533039+zkyary@users.noreply.github.com>
2022-05-21 07:55:40 +0800

    Update Net-info-panel.js

--- Panel/Script/Net-info-panel.js
+++ Panel/Script/Net-info-panel.js
@@ -65,7 +65,7 @@ if (!v4.primaryAddress && !v6.primaryAddress) {
         `魔法位置 : ${getFlagEmoji(info.countryCode)} | ${info.country} - ${info.city
         }`,
       icon: wifi.ssid ? 'waveform' : 'simcard',
-      'icon-color': wifi.ssid ? '#d8bfd8' : '#F9BF45',
+      'icon-color': wifi.ssid ? '#cca3cc' : '#F9BF45',
     });
   });
 }
