d5dfa121acad23c6196184b8c69878d18afff21a
简单帅 <80533039+zkyary@users.noreply.github.com>
2022-06-05 10:35:38 +0800

    Update filebox.js

--- Script/filebox.js
+++ Script/filebox.js
@@ -1,18 +1,18 @@
 /***********************************
 > 應用名稱：filebox
-> 軟件版本：1.1.25
+> 軟件版本：1.1.44(150)
 > 下載地址：https://apps.apple.com/cn/app/id1558391784
 > 腳本作者：Cuttlefish
 > 微信賬號：墨魚手記
 > 解鎖說明：解鎖高級會員權限
-> 更新時間：2022-02-18
+> 更新時間：2022-06-05
 > 通知頻道：https://t.me/ddgksf2021
 > 問題反饋：https://t.me/ddgksf2013_bot
 > 特別說明：本腳本僅供學習交流使用，禁止轉載售賣
  
 [rewrite_local]
 
-# ～ filebox解鎖會員權限（2022-02-18）@ddgksf2013
+# ～ filebox解鎖會員權限（2022-06-05）@ddgksf2013
 ^https?:\/\/api\.revenuecat\.com\/v\d\/subscribers\/(\$RCAnonymousID\%)?(\w)+$ url script-echo-response https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/filebox.js
 ^https?:\/\/api\.revenuecat\.com\/v\d\/receipts$ url script-echo-response https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/filebox.js
 
