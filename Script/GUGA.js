92b581f88694f12f8df69874b74a55810452dcf6
简单帅 <80533039+zkyary@users.noreply.github.com>
2024-10-01 07:04:50 +0800

    GUGA.js

---
+++ Script/GUGA.js
@@ -0,0 +1,20 @@
+
+/******************************
+GUGA （ipad）
+***********************
+
+[rewrite_local]
+^https?:\/\/www\.guga\.co\/api-base\/v2\/(state|preferential-product) url script-response-body https://raw.githubusercontent.com/zkyary/Surge/main/Script/GUGA.js
+
+
+
+[mitm]
+hostname = www.guga.co
+
+*******************************/
+var body = $response.body;
+var data = JSON.parse(body); 
+data.data.level = 1;  
+data.data.expire = 1; 
+body = JSON.stringify(data);
+$done(body);
